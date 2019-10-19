979dfbebe2e886d0aa86f111f69b8f99f030812b
RyanDur <ryan.p.durling@gmail.com>
2019-10-18 20:29:58 -0500

    refactor

--- flows/integration/user_exists_flow_spec.ts
+++ flows/integration/user_exists_flow_spec.ts
@@ -5,24 +5,26 @@ const user: NewUser = {
   name: 'Benedict Cumberbatch'
 };
 
-describe('signing up pre-existing user', () => {
-  it('should not allow a username that already exists', () => {
+const clientError = {
+  onBeforeLoad(win: Window) {
     deferred.resolve({
       json: () => ({username: {value: user.name, validations: ['USERNAME_EXISTS']}}),
       status: 400
     });
-    const signupClientError = cy.visit('/', {
-      onBeforeLoad(win) {
-        cy.stub(win, 'fetch')
-          .withArgs('http://localhost:3001/users')
-          .returns(deferred.promise);
-      }
-    })
+    cy.stub(win, 'fetch')
+      .withArgs('http://localhost:3001/users')
+      .returns(deferred.promise);
+  }
+};
+
+describe('signing up pre-existing user', () => {
+  it('should not allow a username that already exists', () => {
+    const signup = cy.visit('/', clientError)
       .get('#create-user .username input')
       .type(user.name)
       .get('form')
       .submit();
-    signupClientError.get('#current-user .name').should('not.contain', user.name);
-    signupClientError.get('.text-input.username').should('have.class', 'invalid');
+    signup.get('#current-user .name').should('not.contain', user.name);
+    signup.get('.text-input.username').should('have.class', 'invalid');
   });
 });
--- flows/integration/user_flow_spec.ts
+++ flows/integration/user_flow_spec.ts
@@ -2,23 +2,9 @@ import {NewUser} from '../../src/components/user/store/types';
 import deferred from '../support/fake/deferred';
 
 describe('a user', () => {
-  const user: NewUser = {
-    name: 'Benedict Cumberbatch'
-  };
-
   describe('signing up', () => {
     it('should have to create a user', () => {
-      deferred.resolve({
-        json: () => ({name: user.name, id: 'id'}),
-        status: 201
-      });
-      const submit = cy.visit('/', {
-        onBeforeLoad(win) {
-          cy.stub(win, 'fetch')
-            .withArgs('http://localhost:3001/users')
-            .returns(deferred.promise);
-        }
-      })
+      const submit = cy.visit('/', success)
         .get('#create-user .username input')
         .type(user.name)
         .get('form')
@@ -28,3 +14,19 @@ describe('a user', () => {
     });
   });
 });
+
+const user: NewUser = {
+  name: 'Benedict Cumberbatch'
+};
+
+const success = {
+  onBeforeLoad(win: Window) {
+    deferred.resolve({
+      json: () => ({name: user.name, id: 'id'}),
+      status: 201
+    });
+    cy.stub(win, 'fetch')
+      .withArgs('http://localhost:3001/users')
+      .returns(deferred.promise);
+  }
+};
