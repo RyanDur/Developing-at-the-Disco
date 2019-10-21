739a2fafcbc4744636b79fbe169cadb0621b2e38
RyanDur <ryan.p.durling@gmail.com>
2019-10-20 22:07:25 -0500

    refactor style

--- src/components/elements/TextInput/TextInput.tsx
+++ src/components/elements/TextInput/TextInput.tsx
@@ -1,24 +1,15 @@
 import * as React from 'react';
 import {ChangeEvent, useEffect, useState} from 'react';
-import {TextInputProps, TextInputState} from './types';
+import {TextInputProps} from './types';
 import {classes, has} from '../../util/helpers';
 import {MessageList} from '../List';
 import './TextInput.css';
 
 export const TextInput = ({onChange, className, maxLength, placeHolder, errors = {}}: TextInputProps) => {
-  const [{text, isCandidate}, updateText] = useState<TextInputState>({
-    text: '',
-    isCandidate: false
-  });
+  const [text, updateText] = useState('');
+  const [isCandidate, updateCandidate] = useState(false);
 
   useEffect(() => onChange(text));
-
-  const updateValue = ({target}: ChangeEvent<HTMLInputElement>) =>
-    updateText({isCandidate, text: target.value});
-
-  const setCandidate = () => updateText({text, isCandidate: true});
-
-  const checkCandidate = () => updateText({text, isCandidate: has(text)});
 
   return <article className={classes(
     'text-input',
@@ -32,9 +23,9 @@ export const TextInput = ({onChange, className, maxLength, placeHolder, errors =
            id={`create-${className}`}
            className='text'
            value={text}
-           onChange={updateValue}
-           onFocus={setCandidate}
-           onBlur={checkCandidate}
+           onChange={(event) => updateText(event.target.value)}
+           onFocus={() => updateCandidate(true)}
+           onBlur={() => updateCandidate(has(text))}
            maxLength={maxLength}/>
     <label className='max-length'
            htmlFor={`create-${className}`}>{`${text.length}/${maxLength}`}</label>
