36f60f528dd75fc7de535a0c969957bf5b7f3720
RyanDur <ryan.p.durling@gmail.com>
2019-11-17 11:36:56 -0600

    remove unused import

--- src/components/sections/Main/Main.tsx
+++ src/components/sections/Main/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useLayoutEffect, useState} from 'react';
+import {useState} from 'react';
 import {Signup} from '../../user';
 import {classes, not} from '../../util/helpers';
 import './Main.css';
