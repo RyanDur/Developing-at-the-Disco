f635b6158cf92013c73330dfddc4728e4adb63b7
RyanDur <ryan.p.durling@gmail.com>
2019-11-17 11:25:44 -0600

    remove recursive update

--- src/components/sections/Main/Main.tsx
+++ src/components/sections/Main/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {useLayoutEffect, useState} from 'react';
 import {Signup} from '../../user';
 import {classes, not} from '../../util/helpers';
 import './Main.css';
@@ -13,14 +13,17 @@ export const Main = ({hasCurrentUser, onSceneEnd}: MainProps) => {
   const [sceneIsDone, isSceneDone] = useState(false);
   const [offScreen, isOffScreen] = useState(false);
 
-  useEffect(() => {
-    if (offScreen) onSceneEnd(true);
-  });
+  const onAnimationEnd = () => {
+    if (sceneIsDone) {
+      isOffScreen(true);
+      onSceneEnd(true);
+    }
+  };
 
   return <main id='content'>
     {not(offScreen) &&
     <Signup className={classes(hasCurrentUser && sceneIsDone && 'remove')}
-            onAnimationEnd={() => sceneIsDone && isOffScreen(true)}
+            onAnimationEnd={onAnimationEnd}
             onSceneEnd={() => isSceneDone(true)}/>}
   </main>;
 };
