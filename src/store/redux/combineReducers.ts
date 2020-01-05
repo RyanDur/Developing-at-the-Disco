773551d34a9e502ad0df2e2e081003def76f1683
RyanDur <ryan.p.durling@gmail.com>
2020-01-05 10:35:46 -0600

    make it more readable

--- src/store/redux/combineReducers.ts
+++ src/store/redux/combineReducers.ts
@@ -2,8 +2,10 @@ import {Action, AnyAction, AnyState, Reducer, Reducers, State} from './types';
 
 export const combineReducers = <S extends State = AnyState, A extends Action = AnyAction>
 (reducers: Reducers<S, A>): Reducer<S, A> => {
-  const keys: string[] = Object.keys(reducers);
-  return (prevState: S = {} as S, action: A) =>
-    keys.reduce((acc, key) =>
-      ({...acc, [key]: reducers[key](prevState[key], action)}), prevState);
+  const states = Object.keys(reducers);
+  return (previous = {} as S, action) =>
+    states.reduce((updated, state) => ({
+      ...updated,
+      [state]: reducers[state](previous[state], action)
+    }), previous);
 };
